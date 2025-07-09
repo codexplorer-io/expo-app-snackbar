545b9ddc7a56c98e912edead0348b6c83b2575b1
Marko Matic <mail.marko.matic@gmail.com>
2025-07-09 20:26:02 +1000

    - Update safe area for android edge to edge

--- src/component.js
+++ src/component.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Snackbar } from 'react-native-paper';
 import styled from 'styled-components/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { useAppSnackbar, APP_SNACKBAR_POSITION } from './store';
 
-const SnackbarRoot = styled.SafeAreaView`
+const SnackbarRoot = styled(SafeAreaView)`
     position: absolute;
     width: 100%;
     ${({ position }) => position === APP_SNACKBAR_POSITION.top ? 'top: 120px;' : 'bottom: 50px'};
